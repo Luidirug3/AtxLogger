fdfe4cecf25047285f475d03a5ab8522758c7b03
Luigi Di Ruggiero <luigi.diruggiero@aritex-es.com>
2023-09-05 04:16:27 +0200

    si si

--- atx-logger/portfile.cmake
+++ atx-logger/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO Luidirug3/atx-logger
-  REF 714e9a1c19d7cf4400b28a42f40ed213739f85e4
+  REF be578a835a07c92c69cc4af3a1e180388ee37ca9
   HEAD_REF main
 )
 
